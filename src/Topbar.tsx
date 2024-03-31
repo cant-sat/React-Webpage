2013f70a3546977103c883b61432f79ee79ac86d
utelagazkodas <vargamiki53@gmail.com>
2024-03-31 09:22:20 +0200

    top bar changes

--- src/Topbar.tsx
+++ src/Topbar.tsx
@@ -1,8 +1,9 @@
+let buttons : string[]= ["About", "CanSat", "Live"]
+
 
 export function TopBar() {
-    return <body className="flex bg-slate-900 text-red-700 h-20 items-center place-content-center *:m-2">
-        <a href="/#">About</a>
-        <a href="/#">CanSat</a>
-        <a href="/#">Live</a>
+    return <body className="flex bg-slate-900 text-red-700 h-20 items-center place-content-center *:m-2 *:bg-orange-400 *:rounded-2xl *:p-4 *:transition-all">
+        {buttons.map((item) => (
+            <a href={"/#" + item}>{item}</a>))}
     </body>
 }
