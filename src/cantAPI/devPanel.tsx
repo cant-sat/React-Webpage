6e7aad5ec743e94c2f97f82a0e56804a3c295726
Utelagazkodas <vargamiki53@gmail.com>
2024-04-05 10:02:01 +0200

    kill me

--- src/cantAPI/devPanel.tsx
+++ src/cantAPI/devPanel.tsx
@@ -1,11 +1,13 @@
 import connect from "./websocket";
 import { useState } from "react";
 
+
+
 function DeveloperPanel() {
     return (
         <div>
             <hr />
-            <div className="text-center text-4xl m-2 mt-4">Developer panel</div>
+            <div className="m-2 mt-4 text-4xl text-center">Developer panel</div>
             <ConnectForm />
         </div>
     )
@@ -14,9 +16,11 @@ function DeveloperPanel() {
 
 function ConnectForm() {
     const [url, setUrl] = useState("ws://localhost:443")
+    const [connectDisabled, connectDisabledSet] = useState(false)
 
     const handleConnect = (event:React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
+        connectDisabledSet(true)
         connect(url)
     }
 
@@ -25,9 +29,10 @@ function ConnectForm() {
         <form
             onSubmit={handleConnect}
             id="connect"
+            
         >
-            <input type="text" className="textInput" placeholder="URL" id="url" value={url} onChange={(event) => { setUrl(event.target.value) }} />
-            <button type="submit" className="basicButton">
+            <input type="text" className="textInput" placeholder="URL" id="url" value={url} onChange={(event) => { setUrl(event.target.value) }} disabled={connectDisabled}/>
+            <button type="submit" className="basicButton" disabled={connectDisabled}>
                 connect
             </button>
         </form>
--- src/cantAPI/websocket.tsx
+++ src/cantAPI/websocket.tsx
@@ -1,19 +1,29 @@
 export let ws : WebSocket
 
+export let entries : { [id : string] : unknown[]} = {}
+
+
 function connect(url : string){
     ws = new WebSocket(url)
 
-    ws.onmessage = message
-
     ws.onopen = open
 
-    
+    ws.onmessage = message
 
     
 }
 
 function message(ev : MessageEvent<any>){
     let data: {entries : {table : string, values : unknown[]}[]} = JSON.parse(ev.data);
+
+    data.entries.forEach((entrie) => {
+        if(!entries[entrie.table]){
+            entries[entrie.table] = []
+        }
+        entries[entrie.table] =  entries[entrie.table].concat(entrie.values)
+    })
+
+    console.log(entries)
 }
 
 function open(ev : Event) {
